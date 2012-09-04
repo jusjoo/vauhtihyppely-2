0ee21cb80d1d5b3dfb2df273807d17cd7af11774
Henrik Lehtonen <henrik.lehtonen@gmail.com>
2012-09-04 13:05:21 +0300

    lisätty rivi toiselta koneelta

--- Assets/PixelArtImporter.cs
+++ Assets/PixelArtImporter.cs
@@ -29,6 +29,7 @@ public class PixelArtImporter : MonoBehaviour {
 				cube.renderer.material.shader = Shader.Find("Transparent/Diffuse");
                 cube.renderer.material.color = color;
 				// testilisäys versionhallintaan
+				// tämä rivi lisätty toiselta koneelta
 
 				cube.transform.position = new Vector3(this.transform.position.x + x*cubeSize, this.transform.position.y + y*cubeSize, 0);
                 
