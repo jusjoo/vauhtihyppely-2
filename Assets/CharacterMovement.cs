82b8912df12330a9043a6de389d01ed6405c0c95
Teppo Pontelin <teppo.pontelin@gmail.com>
2012-09-12 11:20:28 +0300

    boostit ja jarrutukset

--- Assets/CharacterMovement.cs
+++ Assets/CharacterMovement.cs
@@ -5,38 +5,50 @@ using System.Collections;
 
 public class CharacterMovement : MonoBehaviour {
 	
-	public float speed;
 	public float jumpHeightMultiplier;
 	public float staticSpeed;
+	public float minimumSpeed;
+	public float boostAmount;
+	public float boostDuration;
+	public float boostCooldownTime;
 	
 	private Vector3 movement;
 	private bool jumping;
 	private float jumpHeight;
 	private bool jumpingAllowed;
+	private float boostCooldown;
+	private bool boosting;
 	
+
 	private Rigidbody rigidbody;
     private HUDJumpBooster guiText;
 	private AnimationStateHandler animationHandler;
 
 	
-	// Use this for initialization
 	void Start () {
-		rigidbody = this.GetComponent<Rigidbody>();
-		if ( speed == 0 ) speed = 5;
-        guiText = GameObject.Find("JumpBooster").GetComponent<HUDJumpBooster>();
 
+		rigidbody = this.GetComponent<Rigidbody>();
+        guiText = GameObject.Find("JumpBooster").GetComponent<HUDJumpBooster>();
 		animationHandler = this.GetComponent<AnimationStateHandler>();
 
 	}
 	
-	// Update is called once per frame
+
 	void Update () {
 		
 		// move the character statically first
 		rigidbody.AddForce (staticSpeed*Time.deltaTime, 0, 0);
+
+		// add boost if boost active
+		if (boosting)
+		{
+			boost();
+			if (boostCooldown < boostCooldownTime - boostDuration)
+				boosting = false;
+		}
 		
 		// calculate the movement force given from controllers
-		Vector3 deltaMove = new Vector3(movement.x, movement.y, movement.z) * Time.deltaTime * speed;
+		Vector3 deltaMove = new Vector3(movement.x, movement.y, movement.z) * Time.deltaTime ;
 		rigidbody.AddForce (deltaMove);
 		movement =- deltaMove;
 		
@@ -44,21 +56,53 @@ public class CharacterMovement : MonoBehaviour {
 		if (jumping) {
 			rigidbody.AddForce (0, jumpHeight, 0);
 			jumping = false;
-		}		
+		}
+
+		// count boost cooldown if active
+		if (boostCooldown > 0)
+		{
+			boostCooldown -= Time.deltaTime;
+		}
 	}
 	
 	public void move(float horizontalMovement) {
-		
-		movement.x += horizontalMovement;
+
+		if (horizontalMovement < 0)
+		{
+			slowDown(horizontalMovement);
+		}
+		else if (boostCooldown <= 0)
+		{
+			boostCooldown = boostCooldownTime;
+			boosting = true;
+		}
+
 	}
+
 	
+	private void slowDown(float amount)
+	{
+		Debug.Log("Slowing down!");
+		movement.x += amount;
+		if (movement.x < minimumSpeed)
+			movement.x = minimumSpeed;
+	}
+
+	private void boost()
+	{
+		Debug.Log("Boosting!");
+		movement.x += boostAmount;
+	}
+
 	public void jump(float jumpTimer) {
 
 		if (jumpingAllowed)
 		{
 			jumpHeight = jumpTimer * jumpHeightMultiplier;
 			jumping = true;
-			justJumped();
+			
+			// update gui and animation
+			guiText.setJumpingDone(true);
 			animationHandler.activateJumpAnimation();
 		}
 	}
@@ -68,12 +112,6 @@ public class CharacterMovement : MonoBehaviour {
 		jumpingAllowed = true;
 		animationHandler.deactivateJumpAnimation();
 	}
-
-    public void justJumped(){
-
-        guiText.setJumpingDone(true);
-
-    }
 
 	public void setJumpingAllowed(bool b)
 	{
